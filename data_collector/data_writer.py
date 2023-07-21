a44181a26577188ab681cce87201b92d4f42414e
Mujtaba <38527141+mujtaba-io@users.noreply.github.com>
2023-07-21 20:39:59 +0000

    added words and some code update

--- data_collector/data_writer.py
+++ data_collector/data_writer.py
@@ -14,30 +14,43 @@ import re
 data_folder = "./data/"
 
 
-def refine_text(content: str) -> str:
+def refine_input_text(content: str) -> str:
     content = content.lower()
     # replace all non-alphanumeric characters with empty
     content = re.sub(r'[^a-z0-9]', ' ', content)
     # replace all whitespace characters with single space
     content = re.sub(r'\s+', ' ', content).strip()
+
+    # Remove standalone numbers while preserving numbers within words
+    content = re.sub(r'\b\d+\b(?!-)', '', content)
+
     return content
+
+
+def remove_duplicate_words_in_file(file):
+    with open(file, 'r') as f:
+        content = f.read()
+    words = content.split()
+    unique_words = set()
+    for word in words:
+        unique_words.add(word)
+
+    with open(file, 'w') as f:
+        f.write('\n'.join(unique_words))
+
+
 
 
 def write2dataset(content: str, file_name: str = "no_language_specified_dataset.txt"):
     print("Writing to dataset...")
-    content = refine_text(content)
-    print(content)
+    content = refine_input_text(content)
 
     words = content.split(' ')
 
-    # check if the word is not already in the file_name, add it, else ignore it each word is on a new line
-    file_content = ''
-    with open(data_folder + file_name, 'r') as r_file:
-        file_content = r_file.read()
-
     with open(data_folder + file_name, 'a') as file:
         for word in words:
-            if word not in file_content:
-                file.write(word + '\n')
+            file.write(word + '\n')
+    remove_duplicate_words_in_file(data_folder + file_name)
+    
 
 
